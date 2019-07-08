1b2dc31f1fbee26457fd755147d75bac667a84b7
Amabel <luoweibinb@gmail.com>
2019-07-08 17:55:50 +0900

    feat: initial commit

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+const babel = require('rollup-plugin-babel');
+const pkg = require('./package.json');
+const changeCase = require('change-case');
+const createBanner = require('create-banner');
+
+const name = changeCase.pascalCase(pkg.name);
+
+const banner = createBanner({
+  data: {
+    name: `${name}`,
+    year: '2019-present',
+  }
+})
+
+module.exports = {
+  input: 'src/index.js',
+  output: [
+    {
+      banner,
+      name,
+      file: `dist/umd/index.js`,
+      format: 'umd',
+    }
+  ],
+  plugins: [
+    babel(),
+  ]
+}
